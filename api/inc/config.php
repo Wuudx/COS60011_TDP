e7b6f0f7307c1e54c32daf87ff8ef27288b09b6a
Sebastian Cumins <44050805+Wuudx@users.noreply.github.com>
2022-05-05 20:22:38 +1000

    Delete config.php

--- api/inc/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("DB_HOST", "project-db-instance.c18qa7ggle3x.ap-southeast-2.rds.amazonaws.com");
-define("DB_USERNAME", "admin");
-define("DB_PASSWORD", "PyxWAjZyfX3oGcMupNAi");
-define("DB_DATABASE_NAME", "council_project");
-?>
