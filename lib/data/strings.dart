4b5576d763be04d8937bed216b23f347f04bb0c5
Ban11gers <Ban11gers@users.noreply.github.com>
2022-05-05 17:13:11 +1000

    Adds basic issue form.

--- lib/data/strings.dart
+++ lib/data/strings.dart
@@ -1,3 +1,7 @@
-class Strings{
+class Strings {
+  static const btnSubmit = 'SUBMIT';
+  static const lblCategoryLvl1 = 'Category Lvl1';
+  static const lblDescription = 'Description';
+  static const lblLocation = 'Location';
   static const txtNotImplemented = 'NOT CURRENTLY IMPLEMENTED';
 }
--- lib/pages/issue_form_page.dart
+++ lib/pages/issue_form_page.dart
@@ -1,5 +1,6 @@
 import 'package:council_reporting/data/db.dart';
 import 'package:council_reporting/data/strings.dart';
+import 'package:council_reporting/data/widgets.dart';
 import 'package:flutter/material.dart';
 
 class IssueFormPage extends StatefulWidget {
@@ -13,6 +14,9 @@ class IssueFormPage extends StatefulWidget {
 
 class _IssueFormPageState extends State<IssueFormPage> {
   late User? _user;
+  TextEditingController category1 = TextEditingController();
+  TextEditingController description = TextEditingController();
+  TextEditingController location = TextEditingController();
 
   @override
   void initState() {
@@ -21,16 +25,105 @@ class _IssueFormPageState extends State<IssueFormPage> {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    category1.dispose();
+    description.dispose();
+    location.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 10,
+          ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                Strings.txtNotImplemented,
-                style: TextStyle(fontSize: 30),
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  Strings.lblLocation,
+                                  style: TextStyle(fontSize: 30),
+                                ),
+                                FormWidgets.nameTextField(
+                                  Colors.black,
+                                  controller: location,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  Strings.lblCategoryLvl1,
+                                  style: TextStyle(fontSize: 30),
+                                ),
+                                FormWidgets.nameTextField(
+                                  Colors.black,
+                                  controller: category1,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  Strings.lblDescription,
+                                  style: TextStyle(fontSize: 30),
+                                ),
+                                FormWidgets.nameTextField(
+                                  Colors.black,
+                                  controller: description,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FormWidgets.textButtonTB(
+                Strings.btnSubmit,
+                Colors.white,
+                Colors.green,
+                onClick: () => {print('submitted')},
               ),
             ],
           ),
