dc76e1392032fb505353da9e2501c40590b34c11
Sebastian Cumins <44050805+Wuudx@users.noreply.github.com>
2022-05-09 19:26:04 +1000

    Added additional endpoints

--- api/Controller/IssueController.php
+++ api/Controller/IssueController.php
@@ -2,16 +2,56 @@
 class IssueController extends BaseController {
 
     public function issueAction() {
-        $model = new IssueModel();
-        $strErrorDesc = '';
-        $requestMethod = $_SERVER["REQUEST_METHOD"];
-        $queryParams = $this->getQueryStringParams();
+        $model = new IssueModel(); // new model
+        $strErrorDesc = ''; // String to hold error message
+        $requestMethod = $_SERVER["REQUEST_METHOD"]; // Getting the request method
+        $queryParams = $this->getQueryStringParams(); // Query parameters
+        $uri = $this->getUriSegments(); // Getting the URI
 
+        if (isset($uri[3])) { // The third uri element is set - eg. /api/issues/XXXX
+            if ($uri[3] == 'id') { // ID handling
+                if (strtoupper($requestMethod) != ('GET'||'PATCH'||'DELETE')) { // Checking request method is valid for the endpoint
+                    $requestMethod = null;
+                }
+
+                if (isset($uri[4])) { // Checking the issue id is given as uri[4]
+                    $queryParams['issue_id'] = $uri[4];
+                } else {
+                    $requestMethod = null;
+                }
+            }
+
+            elseif ($uri[3] == 'category') { // Category handling
+                if (strtoupper($requestMethod) != 'GET') { // Valid request method
+                    $requestMethod = null;
+                }
+
+                if (isset($uri[4])) { // Given category?
+                    $queryParams['category_1'] = $uri[4];
+                } else {
+                    $requestMethod = null;
+                }
+            }
+
+            elseif ($uri[3] == 'user') { // User_id handling
+                if (strtoupper($requestMethod) != ('GET'||'POST')) { // Valid request method
+                    $requestMethod = null;
+                }
+
+                if (isset($uri[4])) { // User_id given?
+                    $queryParams['user_id'] = $uri[4];
+                } else {
+                    $requestMethod = null;
+                }
+            }
+        }
+
+        // Assignment of the correct function for the given request method
         if (strtoupper($requestMethod) == 'GET') {
-            try {
+            try { // Try query
                 $data = $model->getData($queryParams);
                 $responseData = json_encode($data);
-            } catch (Error $e) {
+            } catch (Error $e) { // Error
                 $strErrorDesc = $e->getMessage().' Something went wrong! Please contact support.';
                 $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
             }
@@ -27,7 +67,7 @@ class IssueController extends BaseController {
         } 
         elseif (strtoupper($requestMethod) == 'PATCH') {
             try {
-                if (isset($params['issue_id']) && $params['issue_id']) {
+                if (isset($queryParams['issue_id']) && $queryParams['issue_id']) {
                     $data = $model->patchData($queryParams);
                 } else {
                     $strErrorDesc = 'No issue_id field given.';
@@ -41,24 +81,24 @@ class IssueController extends BaseController {
         } 
         elseif (strtoupper($requestMethod) == 'DELETE') {
             try {
-                // TODO
-
+                $data = $model->deleteData($queryParams);
+                $responseData = json_encode($data);
             } catch (Error $e) {
                 $strErrorDesc = $e->getMessage(). ' Something went wrong! Please contact support.';// TODO
                 $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';// TODO
             }
-        } else {
+        } else { // No valid method given - or invalid endpoint format
             $strErrorDesc = 'Method not supported';
-            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+            $strErrorHeader = 'HTTP/1.1 400 Bad Request';
         }
  
         // send output
-        if (!$strErrorDesc) {
+        if (!$strErrorDesc) { // No error message = success
             $this->sendOutput(
                 $responseData,
                 array('Content-Type: application/json', 'HTTP/1.1 200 OK')
             );
-        } else {
+        } else { // Error somewhere
             $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
                 array('Content-Type: application/json', $strErrorHeader)
             );
