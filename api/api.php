ac8c5caf154e35ce458960bb46fc5e047e318ba5
Sebastian Cumins <44050805+Wuudx@users.noreply.github.com>
2022-05-12 19:48:44 +1000

    Update api.php
    Added support for /api/user/ endpoints

--- api/api.php
+++ api/api.php
@@ -1,11 +1,15 @@
 <?php
 require __DIR__ . "/inc/bootstrap.php";
 require PROJECT_ROOT_PATH . "/Controller/IssueController.php";
+require PROJECT_ROOT_PATH . "/Controller/UserController.php";
 
 
 if (isset($uri[2]) && $uri[2] == 'issue') { // Issue Model
     $controller = new IssueController();
     $controller->issueAction();
+} elseif (isset($uri[2]) && $uri[2] == 'user') {
+    $controller = new UserController();
+    $controller->userAction();
 } else { // Invalid endpoint - no model found
     $strErrorDesc = 'Method not supported';
     $strErrorHeader = 'HTTP/1.1 400 Bad Request';
