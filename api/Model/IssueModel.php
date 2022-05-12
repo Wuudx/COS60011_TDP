0c7cd211bc8b2dee39e47833adb8a8d8eb61c793
Sebastian Cumins <44050805+Wuudx@users.noreply.github.com>
2022-05-12 19:55:54 +1000

    Minor bug fixes

--- api/Model/IssueModel.php
+++ api/Model/IssueModel.php
@@ -3,8 +3,8 @@ require_once PROJECT_ROOT_PATH . '/Model/Database.php';
  
 class IssueModel extends Database {
 
-    public function getData($params = []) {
-        $query = 'SELECT * FROM issue'; // Initial query - will add to this
+    public function getIssue($params = []) {
+        $query = 'SELECT * FROM `issue`'; // Initial query - will add to this
         $limit = $params['limit'];
         $paramNames = []; // Temp array to hold the param argument names
 
@@ -12,10 +12,10 @@ class IssueModel extends Database {
         
         if ($params) { // If there are WHERE arguments appending that to the query
             foreach ($params as $key=>$value) { // Filling the temp array with where clause variables
-                array_push($paramNames, $key . '=?'); // Array push var=?
+                array_push($paramNames, $key . '`=?'); // Array push var=?
             }
             
-            $query .= ' WHERE ' . implode(' AND ', $paramNames);
+            $query .= ' WHERE `' . implode(' AND `', $paramNames);
         }
 
         if (isset($limit) && $limit) { // If a limit param is given set it to that
@@ -24,21 +24,21 @@ class IssueModel extends Database {
             $params = $params + array('limit'=>10);
         }
 
-        $query .= ' ORDER BY issue_id ASC LIMIT ?'; // Append the limit to the query
+        $query .= ' ORDER BY `issue_id` ASC LIMIT ?'; // Append the limit to the query
 
         return $this->select($query, array_values($params));
     }
 
 
-    public function postData($params = []) {
+    public function postIssue($params = []) {
         // Generating query
-        $query = 'INSERT INTO issue (' . implode(', ', array_keys($params)) . ') VALUES (' . implode(', ', array_fill(0, count($params), '?')) . ')';
+        $query = 'INSERT INTO `issue` (`' . implode('`, `', array_keys($params)) . '`) VALUES (' . implode(', ', array_fill(0, count($params), '?')) . ')';
 
         return $this->insert($query, array_values($params));
     }
 
 
-    public function patchData($params = []) {
+    public function patchIssue($params = []) {
         $query = 'UPDATE issue'; // Initial query - will add to this
         $issueID = $params['issue_id']; // Isolation of the issue id
         $paramNames = []; // temp array to hold paramnames
@@ -61,7 +61,7 @@ class IssueModel extends Database {
     }
 
 
-    public function deleteData($params = []) {
+    public function deleteIssue($params = []) {
         $query = 'DELETE FROM issue';
         $paramNames = [];
 
