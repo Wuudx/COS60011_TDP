233300d1d0f649fa4ca9f234aed2594ed56fff40
Sebastian Cumins <44050805+Wuudx@users.noreply.github.com>
2022-05-12 19:49:18 +1000

    Update bootstrap.php

--- api/inc/bootstrap.php
+++ api/inc/bootstrap.php
@@ -9,4 +9,7 @@ require_once PROJECT_ROOT_PATH . "/Controller/BaseController.php";
  
 // include the use model file
 require_once PROJECT_ROOT_PATH . "/Model/IssueModel.php";
+
+// include the use model file
+require_once PROJECT_ROOT_PATH . "/Model/UserModel.php";
 ?>
