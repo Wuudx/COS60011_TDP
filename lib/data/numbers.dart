7f2bc633825f6df8e1ed9b1a92c0076675644d98
Ban11gers <Ban11gers@users.noreply.github.com>
2022-05-23 01:10:53 +1000

    Adds a coordinate generator for places around swinburne.

--- lib/data/numbers.dart
+++ lib/data/numbers.dart
@@ -1,4 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
+
+class FakeLocation {
+  static double _randomDouble(Random source, double min, double max) {
+    return source.nextDouble() * (max - min) + min;
+  }
+
+  static LatLng random() {
+    Random random = Random();
+    return LatLng(
+      _randomDouble(random, -37.823118, -37.819758),
+      _randomDouble(random, 145.035384, 145.040224),
+    );
+  }
+}
 
 class Dimensions {
   static const btnHeight60 = 60.0;
