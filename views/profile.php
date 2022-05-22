4cfb7131b76d0fc1468c8c0d4c8645c4668e8e90
Kevin Bronowicki <kev.bronowicki@live.com>
2022-05-22 22:01:30 +1000

    Added profile page to view user details

--- views/profile.php
+++ views/profile.php
@@ -1,3 +1,15 @@
+<?php
+	require_once('api/inc/bootstrap.php');
+	require_once('api/Model/StaffModel.php');
+	
+	$staffModel = new StaffModel();
+    $data = $staffModel->getUserByEmail($_SESSION["email"]);
+
+	if (count($data) > 0)
+	{
+		$user = $data[0];
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>  
@@ -6,7 +18,23 @@
 	<body>
 		<?php require_once('includes/navbar.php'); ?>
 		<div class="container-fluid mt-3">
-		  <h3>Profile page</h3>
+		  <h3 class="ml-3 mb-3">Profile</h3>
+		  <div class="card ml-3 w-50">
+			<div class="card-body">
+				<div>
+					<h6>Email:</h6>
+					<p><?php echo $user['email']; ?></p>
+				</div>
+				<div>
+					<h6>Name:</h6>
+					<p><?php echo $user['first_name'] . ' ' . $user['last_name']; ?></p>
+				</div>
+				<div>
+					<h6>Password:</h6>
+					<p><?php echo str_repeat("*", strlen($user['password'])); ?></p>
+				</div>
+			</div>
+		  </div>
 		</div>
 		<?php require_once('includes/footer.php'); ?>
 	</body>
