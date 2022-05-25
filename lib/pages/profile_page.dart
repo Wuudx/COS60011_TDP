c44324b31b7d28bb86beef04446dad82b710e494
Ban11gers <Ban11gers@users.noreply.github.com>
2022-05-25 19:57:02 +1000

    Adds Hariesh's version of profile page.

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,40 +1,191 @@
-import 'package:council_reporting/data/db.dart';
-import 'package:council_reporting/data/strings.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
-  final User user;
-
-  const ProfilePage({required this.user, Key? key}) : super(key: key);
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  late User _user;
-
-  @override
-  void initState() {
-    super.initState();
-    _user = widget.user;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                Strings.txtNotImplemented,
-                style: TextStyle(fontSize: 30),
+      //Navigation bar
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: const Text(
+          "Profile",
+          style: TextStyle(fontSize: 29),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        //Profile Slab displaying User Information
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 40),
+              height: 330,
+              decoration: const BoxDecoration(
+                color: Color(0xFF607D8B),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(20),
+                ),
               ),
-            ],
-          ),
+              //Profile Picture linked by network image
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png",
+                        ),
+                        radius: 50,
+                      ),
+                      Positioned(
+                        bottom: 0.0,
+                        right: 0.0,
+                        child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                          child: const Icon(
+                            Icons.edit,
+                            color: Color(0xFF607D8B),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Full Name",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Divider(
+                    thickness: 1,
+                    indent: 20,
+                    endIndent: 20,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          //Displays user information Likes and Ranking within leaderboard
+                          Text(
+                            "45",
+                            style: TextStyle(
+                              fontSize: 42,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white.withOpacity(0.9),
+                            ),
+                          ),
+                          const Text(
+                            "Rank",
+                            style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "335",
+                            style: TextStyle(
+                                fontSize: 42,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white.withOpacity(
+                                  0.9,
+                                )),
+                          ),
+                          const Text(
+                            "Likes",
+                            style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+
+            //Displays the leaderboard
+            const Text(
+              "Leaderboard",
+              style: TextStyle(fontSize: 20),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: SizedBox(
+                height: 300,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Row(
+                        children: const [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              "https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png",
+                            ),
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          Text("Full Name")
+                        ],
+                      ),
+                      leading: Text(
+                        "#${index + 1}",
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text(
+                        (20000 / (index + 1)).toString().substring(0, 5),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) => const Divider(
+                    thickness: 1,
+                    color: Color(0xFF607D8B),
+                    indent: 10,
+                    endIndent: 10,
+                  ),
+                  itemCount: 20,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
